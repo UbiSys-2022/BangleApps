e6d36439e3e6eb9e3c2157a94bdfa5ffa75c092a
Andrey Panteleyev <andrey.panteleyev@ltnglobal.com>
2022-06-30 23:04:51 +0200

    patient: fix method spelling mistake

--- apps/patient/app.js
+++ apps/patient/app.js
@@ -71,7 +71,7 @@ function drawHRM() {
   if (heartRate > 120) g.drawString("Pulse too high!", 120, 200, true);
   /* Bradycardia
   Slow, resting heart rate and commonly normal during sleep, or for resting athletes. More tricky do detect, because if loop checks for pulse under 60, every sleeping person might trigger an alarm. For testing check for values lower than 40. */
-  if (heartRate < 40) g.drawstring("Pulse too low!", 120, 200, true);
+  if (heartRate < 40) g.drawString("Pulse too low!", 120, 200, true);
 }
 
 function fileWrite(bpm) {
